741b48a98ecbf0a0ac4631584167b5dd0b0ba4e1
maks <maks-lapin-1998@.ru>
2023-04-23 22:31:54 +0400

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine("Введите первое число "); 
+string a = Console.ReadLine(); 
+int c = int.Parse(a);
+
+System.Console.WriteLine("Введите Второе число ");
+string b = Console.ReadLine();
+int d = int.Parse(b);
+
+if (c>d)
+{
+    System.Console.WriteLine($"Большее число {c}");
+    System.Console.WriteLine($"Меньшее число {d}");
+}
+else if (d>c)
+{
+    System.Console.WriteLine($"Большее число {d}");
+    System.Console.WriteLine($"Меньшее число {c}");
+}
+else {
+    System.Console.WriteLine("Числа равны");
+}
