f788f46ec8107b303ff6a5fe3a5b18a8f16e4c6c
maks <maks-lapin-1998@.ru>
2023-04-23 22:37:01 +0400

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,30 @@
+﻿System.Console.WriteLine("Введите первое число "); 
+string a = Console.ReadLine(); 
+int d = int.Parse(a);
+
+System.Console.WriteLine("Введите второе число "); 
+string b = Console.ReadLine();
+int e = int.Parse(b);
+
+System.Console.WriteLine("Введите третье число "); 
+string c = Console.ReadLine();
+int f = int.Parse(c);
+
+int max = 0;
+
+if (max < d)
+{
+    max = d;
+}
+
+if (max < e)
+{
+    max = e;
+}
+
+if (max < f)
+{
+    max = f;
+}
+
+System.Console.WriteLine($"Максимальное число : {max}");
